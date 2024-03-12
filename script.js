4d7fe0d845038041f10ecdf7b0ef404ec7c7a72a
AlirezaZahedi <154251208+alireza-z-5700@users.noreply.github.com>
2024-03-13 00:20:06 +0330

    q

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-console.log("heytfdlkuw  yedfgo");
+console.log('heytfdlkuw  yedfgo');
 
 // set year
 
-const yearEL = document.querySelector(".year");
+const yearEL = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEL.textContent = currentYear;
 
 //mobile nav
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".open-on-top");
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.open-on-top');
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
 });
 
 // smoth
 
-const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll('a:link');
 
 allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
+  link.addEventListener('click', function (e) {
     e.preventDefault();
-    const href = link.getAttribute("href");
+    const href = link.getAttribute('href');
 
     // Scroll back to top
-    if (href === "#")
+    if (href === '#')
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
 
     // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
+    if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
     }
 
     // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
   });
 });
